083f5d4371f3b4524edfbf322bbb05b28e1496bb
codemmk <2107228359@qq.com>
2024-09-08 11:42:38 +0800

    style:tailwind and less

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['nuxt-graphql-client'],
+  modules: ['nuxt-graphql-client', '@nuxtjs/tailwindcss'],
   runtimeConfig: {
     public: {
       GQL_HOST: 'http://admin.200205.net/graphql' // overwritten by process.env.GQL_HOST
