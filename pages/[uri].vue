6938757a7baa8be3ed465911382fb15fa1ec9c09
codemmk <2107228359@qq.com>
2024-09-15 19:40:47 +0800

    SEO

--- pages/[uri].vue
+++ pages/[uri].vue
@@ -7,36 +7,44 @@ try {
 } catch (error) {
   console.log(error)
 }
-const article = result.data
-console.log(article)
+
+const article = ref(null)
+article.value = result.data
+console.log(article.value)
+
+useSeoMeta({
+  title: article.value.title,
+  description: article.value.excerpt
+})
 </script>
 
 <template>
   <div class="article p-8 bg-white">
-    <div class="flex items-center">
-      <!-- 有缩略图的显示方式 -->
-      <template v-if="article.featuredImage">
-        <div class="w-48 h-48 flex justify-center items-center mr-8 overflow-hidden">
-          <img :src="article.featuredImage.node.sourceUrl" :alt="article.featuredImage.node.title">
-        </div>
-        <div>
-          <div class="text-lg mb-4">{{ article.title }}</div>
-          <div>
-            <NuxtLink :to="article.author.node.uri" class="flex items-center mb-4">
-              <img class="w-8 h-8 rounded-full mr-2" :src="article.author.node.avatar.url" :alt="article.author.node.name">
-              <div>
-                {{ article.author.node.name }}
-              </div>
-            </NuxtLink>
-          </div>
-          <div class="categories  mb-4">
-            <span>分类: </span>
-            <NuxtLink v-for="item in article.categories.nodes" :key="item.id" :to="item.uri" class="mr-2">
-              {{ item.name }}
-            </NuxtLink>
+    <!-- 有缩略图的显示方式 -->
+    <template v-if="article.featuredImage">
+        <div class="flex items-center pb-8">
+          <div class="w-48 h-48 flex justify-center items-center mr-8 overflow-hidden">
+            <img :src="article.featuredImage.node.sourceUrl" :alt="article.featuredImage.node.title">
           </div>
           <div>
-            <time :datetime="$dayjs(article.date).format('YYYY/MM/DD').toString()">{{ $dayjs(article.date).format('YYYY-MM-DD').toString() }} </time>
+            <div class="text-lg mb-4">{{ article.title }}</div>
+            <div>
+              <NuxtLink :to="article.author.node.uri" class="flex items-center mb-4">
+                <img class="w-8 h-8 rounded-full mr-2" :src="article.author.node.avatar.url" :alt="article.author.node.name">
+                <div>
+                  {{ article.author.node.name }}
+                </div>
+              </NuxtLink>
+            </div>
+            <div class="categories  mb-4">
+              <span>分类: </span>
+              <NuxtLink v-for="item in article.categories.nodes" :key="item.id" :to="item.uri" class="mr-2">
+                {{ item.name }}
+              </NuxtLink>
+            </div>
+            <div>
+              <time :datetime="$dayjs(article.date).format('YYYY/MM/DD').toString()">{{ $dayjs(article.date).format('YYYY-MM-DD').toString() }} </time>
+            </div>
           </div>
         </div>
       </template>
@@ -63,9 +71,8 @@ console.log(article)
           </div>
         </div>
       </template>
-    </div>
     <article class="prose max-w-full mb-4" v-html="article.content"></article>
-    <div class="shadow-md my-8 rounded-md">
+    <!-- <div class="shadow-md my-8 rounded-md">
       <div class="p-4">Comments </div>
       <div class="p-4">
         <div v-for="item in article.comments.nodes">
@@ -81,7 +88,7 @@ console.log(article)
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
