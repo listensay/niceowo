01abb19dabe90cf360eb93ca8f4c1cef2ae83780
codemmk <2107228359@qq.com>
2024-10-10 17:54:57 +0800

    add: 更多导航组件

---
+++ components/MoreNav.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: '标题'
+  },
+  link: {
+    type: String,
+    default: '/'
+  }
+})
+</script>
+
+<template>
+  <div class="more-nav">
+    <div class="mb-6 flex justify-between items-center">
+      <div class="text-lg">{{ title }} </div>
+      <NuxtLink :to="link" class="block text-sm">更多内容</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped></style>
